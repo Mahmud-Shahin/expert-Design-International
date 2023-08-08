4aac14dffee44b4daaba81defaddcdce02222d15
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-08-09 01:15:22 +0600

    first commit

---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Slider from "./Slider";
+
+const Banner = () => {
+  return (
+    <div className="hero py-6 ">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Slider></Slider>
+        <div>
+          <h1 className="text-4xl font-bold ">
+            Cheap Photo Editing Service Provider Especially for E-commerce and
+            Product Photos
+          </h1>
+          <p className="py-6">
+            Are you looking for a graphic design or image manipulation company
+            to help you with your ecommerce product photographs or other
+            personal projects? If that’s the case, let us take this occasion to
+            extend a hearty welcome to you as we are the best photo editing and
+            clipping path Service Company you will find in the market.
+          </p>
+          <button className=" btn btn-primary">Get a quote</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home/Slider.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Slider.css";
+import SliderOne from "../../assets/images/Slider (1).webp";
+import SliderTwo from "../../assets/images/Slider (2).webp";
+import backgroundremoval from "../../assets/images/background-removal-service.webp";
+import removalshadow from "../../assets/images/background-removal-shadow-service.webp";
+
+const Slider = () => {
+  return (
+    <div className="carousel w-full slider">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={SliderOne} alt="" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={SliderTwo} alt="" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={backgroundremoval} alt="" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={removalshadow} alt="" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
